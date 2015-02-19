cfd9f7096293711ef51fbec072b7c9223f48e04a
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2015-02-18 21:14:49 -0800

    fix numpy uninstall

--- numpy/tools/chocolateyUninstall.ps1
+++ numpy/tools/chocolateyUninstall.ps1
@@ -1,7 +1,7 @@
-$packageName = 'wxpython'
+$packageName = 'numpy'
 $installerType = 'EXE' 
-$file = "C:\tools\python2-x86_32\Removenumpy.exe"
-$notSoSilentArgs = '-u C:\tools\python2-x86_32\numpy-wininst.log'
+$file = "C:\Python27\Removenumpy.exe"
+$notSoSilentArgs = '-u C:\Python27\numpy-wininst.log'
 $validExitCodes = @(0) 
 
 Uninstall-ChocolateyPackage "$packageName" "$installerType" "$notSoSilentArgs" "$file" -validExitCodes $validExitCodes
